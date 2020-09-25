b3c28e7f0f72d4ffb7e3f631f65c95a3dd8224c0
Ahmed Elghafar <Ahmed.Elghafar@AVELABS.CAI>
2020-09-25 11:52:16 +0200

    Adding First try

--- 01_Sorting_Algorithms/01_Bubble_Sort/Bubble_Sort/main.c
+++ 01_Sorting_Algorithms/01_Bubble_Sort/Bubble_Sort/main.c
@@ -26,13 +26,14 @@ uint32_t my_array[MAX_ARRAY] = ARRAY_INIT;
 */
 
 void swap(uint32_t *first, uint32_t *second);
-fun_ret_t bubble_Sort(uint32_t *my_array, uint32_t arr_length);
+fun_ret_t bubble_sort(uint32_t *my_array, uint32_t arr_length);
+fun_ret_t bubble_sort_optimized(uint32_t *my_array, uint32_t arr_length);
 fun_ret_t print_array(uint32_t *my_array, uint32_t arr_length);
 
 int main()
 {
     print_array(my_array, MAX_ARRAY);
-    bubble_Sort(my_array, MAX_ARRAY);
+    bubble_sort_optimized(my_array, MAX_ARRAY);
     print_array(my_array, MAX_ARRAY);
     return 0;
 }
@@ -55,7 +56,7 @@ void swap(uint32_t *first, uint32_t *second){
   * @param  arr_length Number of elements in the Array.
   * @retval Status if the function execution
   */
-fun_ret_t bubble_Sort(uint32_t *my_array, uint32_t arr_length){
+fun_ret_t bubble_sort(uint32_t *my_array, uint32_t arr_length){
     uint32_t l_counter1 = ZERO_INIT, l_counter2 = ZERO_INIT;
     if(arr_length < 0){
         return R_NOK;
@@ -67,6 +68,36 @@ fun_ret_t bubble_Sort(uint32_t *my_array, uint32_t arr_length){
                     swap(&my_array[l_counter2], &my_array[l_counter2+1]);
                 }
             }
+            printf("Inner : "); print_array(my_array, MAX_ARRAY);
+        }
+    }
+    return R_OK;
+}
+
+/**
+  * @brief  Implement Bubble Sorting
+  * @param  my_array Pointer to the array.
+  * @param  arr_length Number of elements in the Array.
+  * @retval Status if the function execution
+  */
+fun_ret_t bubble_sort_optimized(uint32_t *my_array, uint32_t arr_length){
+    uint32_t l_counter1 = ZERO_INIT, l_counter2 = ZERO_INIT;
+    uint32_t l_swap_flag = 0;
+    if(arr_length < 0){
+        return R_NOK;
+    }
+    else{
+        for(l_counter1=ZERO_INIT; l_counter1<arr_length-1; ++l_counter1){
+            for(l_counter2=ZERO_INIT; l_counter2<arr_length-l_counter1-1; ++l_counter2){
+                if(my_array[l_counter2] > my_array[l_counter2+1]){
+                    swap(&my_array[l_counter2], &my_array[l_counter2+1]);
+                    l_swap_flag = 1;
+                }
+            }
+            if(0 == l_swap_flag){
+                break;
+            }
+            else{}
             printf("Inner : "); print_array(my_array, MAX_ARRAY);
         }
     }
